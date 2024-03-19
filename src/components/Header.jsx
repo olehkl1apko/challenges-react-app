b3414aac4019eca47bea81f7c2edbf12115f7193
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-19 08:38:20 +0200

    ADD: go-back btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
-import NewChallenge from './NewChallenge.jsx';
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
+  const navigate = useNavigate();
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
 
   function handleStartAddNewChallenge() {
@@ -22,14 +24,24 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          transition={{ type: 'spring', stiffness: 500 }}
-          onClick={handleStartAddNewChallenge}
-          className="button"
-        >
-          Add Challenge
-        </motion.button>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 500 }}
+            onClick={handleStartAddNewChallenge}
+            className="button"
+          >
+            Add Challenge
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 500 }}
+            onClick={() => navigate("/")}
+            className="button go-back"
+          >
+            Go back
+          </motion.button>
+        </div>
       </header>
     </>
   );
